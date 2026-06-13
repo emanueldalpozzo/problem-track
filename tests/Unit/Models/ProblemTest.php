b54120d080dd71c56b96d039153b04f64cea13fd
emanuel <emanuelx789@gmail.com>
2026-06-13 00:30:44 -0300

    Write some controllers test

--- tests/Unit/Models/ProblemTest.php
+++ tests/Unit/Models/ProblemTest.php
@@ -3,11 +3,9 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Problem;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
-class ProblemTest extends TestCase {
-
-const DB_PATH = '/var/www/database/problems.txt';
+class ProblemTest extends TestCase{
 
 public function test_can_set_tytle(){
     $problem = new Problem(title: 'Problem 1');
@@ -15,11 +13,23 @@ public function test_can_set_tytle(){
     $this -> assertEquals('Problem 1', $problem -> getTitle());
 }
 
-public function test_has_to_create_new_problem(){
-    $problem = new Problem(title: 'Problem 1');
-    $inicialCount = count(Problem::all());
 
-    $this -> assertTrue($problem -> save());
-    $this -> assertCount($inicialCount + 1, Problem::all());
+
+
+public function test_has_to_create_new_problem(){
+    $problem = new Problem(title:'Problem 1');
+    
+    $this->assertTrue($problem->save());
+    $this->assertCount(1, Problem::all());
 }
+
+// public function test_has_to_create_new_problem(){
+//     $problem = new Problem(title: 'Problem 1');
+//     $inicialCount = count(Problem::all());
+
+//     $this -> assertTrue($problem -> save());
+//     $this -> assertCount($inicialCount + 1, Problem::all());
+// }
+
+
 }
