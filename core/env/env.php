94be96d3860b5bbcc15ae1e06db924688e779f02
emanuel <emanuelx789@gmail.com>
2026-06-13 00:30:51 -0300

    Write some controllers test

---
+++ core/env/env.php
@@ -0,0 +1,8 @@
+//Carrega o .env e tranforma em variáveis de ambiente\\
+<?php
+
+    $envs = parse_ini_file('/var/www/.env');
+
+    foreach ($envs as $key => $value){
+        $_ENV[$key] = $value;
+    }
