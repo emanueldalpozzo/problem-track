4c5e59b89e01c2bd91fba20a7c5d5e6631070d2e
emanuel <emanuelx789@gmail.com>
2026-06-18 01:16:34 -0300

    Refactor core env to OO

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,14 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php'; //Autoload do composer
 
-use Core\Constants\Constants;
+use Core\Env\EnvLoader;
 use Core\Errors\ErrorsHandler;
 
 ErrorsHandler::init();
+EnvLoader::init();
 
-Constants::rootPath();
 // require_once __DIR__ . '/../core/constants/general.php';
-require_once Constants::rootPath()->join( '/core/env/env.php');
 // require_once ROOT_PATH . '/core/debug/functions.php';
 
-//require_once ROOT_PATH . '/core/errors/handler.php';
+
---
+++ core/Env/EnvLoader.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core\Env;
+
+use Core\Constants\Constants;
+
+class EnvLoader
+{
+
+    static function init()
+    {
+        $envs = parse_ini_file(Constants::rootPath()->join('.env'));
+
+        foreach ($envs as $key => $value) {
+            $_ENV[$key] = $value;
+        }
+    }
+}
