ab0bb7a6db892ec4ed23691018ab6d609beae17d
emanuel <emanuelx789@gmail.com>
2026-06-19 01:12:14 -0300

    Fixed php stan issues

---
+++ Core/Constants/Constants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Constants;
+
+class Constants
+{
+    public static function rootPath(): StringPath
+    {
+
+        return new StringPath(dirname(__DIR__,2));
+
+    }
+
+    public static function databasePath(): StringPath{
+        return self::rootPath()->join('/database');
+    }
+
+}
+ 
---
+++ Core/Env/EnvLoader.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core\Env;
+
+use Core\Constants\Constants;
+
+class EnvLoader
+{
+    
+
+    static function init(): void
+    {
+        $envs = parse_ini_file(Constants::rootPath()->join('.env'));
+
+        foreach ($envs as $key => $value) {
+            $_ENV[$key] = $value;
+        }
+    }
+}
